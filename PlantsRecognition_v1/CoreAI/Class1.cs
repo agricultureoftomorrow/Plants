79995b5bb610b484ef9749e2074130d6110a1369
Patryk Apriasz <patrykapriasz@gmail.com>
2018-04-15 11:34:55 +0200

    Library and unit tests projects
    Add to solution library for AI and unit tests projects

---
+++ PlantsRecognition_v1/CoreAI/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CoreAI
+{
+    public class Class1
+    {
+    }
+}
---
+++ PlantsRecognition_v1/PlantsRecognition_v1.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlantsRecognition_v1.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
